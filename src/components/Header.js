273ecbcc6a969f20e2248d61655a64d95cced3c9
Muhammed Fatih Candan <mfatihcandan@gmail.com>
2022-05-24 00:53:39 +0300

    fundamentals

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { StyledHeader } from "./styles/Header.styled";
+
+const Header = () => {
+  return (
+    <StyledHeader>
+      <h1>Header</h1>
+    </StyledHeader>
+  );
+};
+
+export default Header;
